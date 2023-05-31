c7230286507bc4bb8d206c8e7d33272a23b946bf
sas8254 <shashibhushansingh11@gmail.com>
2023-05-31 16:00:24 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Slot from "./slot";
+
+function App() {
+  return (
+    <div>
+      <Slot val1="💸" val2="💸" val3="💸" />
+      <Slot val1="🍒" val2="🍌" val3="🍒" />
+    </div>
+  );
+}
+
+export default App;
