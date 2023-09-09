fdf672f252ff78c8af1a28d7b24bea1981104ed5
sas8254 <shashibhushansingh11@gmail.com>
2023-09-09 11:14:18 +0530

    Coding Exercise 10: Exercise: Updating State Based On Older State

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  const [isValid, setIsValid] = React.useState(false);
-  const handleValid = (e) => {
-    if (e.target.value.trim().length > 3) {
-      setIsValid(true);
-    } else {
-      setIsValid(false);
-    }
+  let [count, setCount] = React.useState(0);
+  const handleClick = (e) => {
+    setCount(() => {
+      return count + 1;
+    });
   };
   return (
-    <form>
-      <label>Your message</label>
-      <input type="text" onChange={handleValid} />
-      <p>{isValid ? "Valid message" : "Invalid message"}</p>
-    </form>
+    <div>
+      <p id="counter">{count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
   );
 }
 
