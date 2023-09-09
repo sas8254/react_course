5031914e72f4831e7caef514393708fbf1e186f0
sas8254 <shashibhushansingh11@gmail.com>
2023-09-09 10:59:49 +0530

    Coding Exercise 9: Exercise: Using State with Form Inputs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,21 @@
 import "./App.css";
-import componentsImage from "./assets/images/components.png";
-import stateImage from "./assets/images/state.png";
-import eventsImage from "./assets/images/events.png";
-import Header from "./components/Header";
-import Concepts from "./Concepts";
-
-const concepts = [
-  {
-    title: "Components",
-    image: componentsImage,
-    description:
-      "Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.",
-  },
-  {
-    title: "State",
-    image: stateImage,
-    description:
-      "State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.",
-  },
-  {
-    title: "Events",
-    image: eventsImage,
-    description:
-      "Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.",
-  },
-];
+import React from "react";
 
 function App() {
+  const [isValid, setIsValid] = React.useState(false);
+  const handleValid = (e) => {
+    if (e.target.value.trim().length > 3) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
   return (
-    <div>
-      <Header />
-      <Concepts concepts={concepts} />
-    </div>
+    <form>
+      <label>Your message</label>
+      <input type="text" onChange={handleValid} />
+      <p>{isValid ? "Valid message" : "Invalid message"}</p>
+    </form>
   );
 }
 
--- src/Concept.jsx
+++
@@ -1,9 +0,0 @@
-export default function Concept({ concept }) {
-  return (
-    <div className="concept">
-      <img src={concept.image} alt={concept.title} />
-      <h2>{concept.title}</h2>
-      <p>{concept.description}</p>
-    </div>
-  );
-}
